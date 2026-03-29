d0a1ac3688c7814151d87fa836137f1a0af6a3d0
Freddoda <alfredgormly@gmail.com>
2026-03-29 17:27:24 +1100

    added bird

--- C/Flappy.c
+++ C/Flappy.c
@@ -15,14 +15,95 @@ struct timespec timespec_subtract(struct timespec* a, struct timespec* b){
     return result;
 }
 
-void update(const bool* keys){
-    if (keys[SDL_SCANCODE_W]){
-        printf("w");
+typedef struct { int r, g, b; } Colour;
+
+void renderCircle(SDL_Renderer* renderer, int x, int y, int rad, Colour colour){
+    SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, 255);
+    for (int x2 = -rad; x2<=rad; x2++){
+        for (int y2 = -rad; y2<=rad; y2++){
+            if ((x2 * x2 + y2 * y2)<(rad*rad)){
+                SDL_RenderPoint(renderer,x+x2,y+y2);
+            }
+        }
     }
 }
 
-void draw(SDL_Renderer* renderer){
+typedef struct {
+    int x;
+    float y;
+    int rad;
+    float ysp;
+    int jtimer;
+} Bird;
 
+Bird* newBird(){
+        Bird* b = malloc(sizeof(Bird));
+    if (b) {
+        b->x=200;
+        b->y=400;
+        b->rad=25;
+        b->ysp=0;
+        b->jtimer=0;
+    }
+    return b;
+}
+
+void bird_jump(Bird* bird, const bool* keys){
+    if (bird->jtimer>0){
+        bird->jtimer--;
+    } else if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_SPACE]){
+        bird->jtimer=30;
+        bird->ysp=-11;
+    }
+    if (bird->ysp<10){
+        bird->ysp+=0.4;
+    }
+    bird->y += bird->ysp;
+}
+
+bool bird_collide(Bird* bird){
+    if (bird->y-bird->rad<0 || bird->y+bird->rad>940){
+        return true;
+    }
+    return false;
+}
+
+void bird_reset(Bird* bird){
+    bird->ysp=0;
+    bird->y=400;
+    bird->jtimer=0;
+}
+
+void bird_draw(Bird* bird, SDL_Renderer* renderer){
+    renderCircle(renderer, bird->x, bird->y, bird->rad, (Colour){255,255,0});
+}
+
+void update(const bool* keys, bool* playing, bool* playstart, Bird* bird){
+    if (*playing){
+        bird_jump(bird,keys);
+        if (bird_collide(bird)){
+            *playing = false;
+            *playstart = false;
+            bird_reset(bird);
+        }
+    } else {
+        if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_SPACE]){
+            if (*playstart){
+                *playing = true;
+            }
+        } else {
+            *playstart = true;
+        }
+    }
+}
+
+void draw(SDL_Renderer* renderer, Bird* bird){
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    bird_draw(bird, renderer);
+
+    SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char* args[]){
@@ -35,12 +116,18 @@ int main(int argc, char* args[]){
     const bool *keys = SDL_GetKeyboardState(NULL);
 
     //framerate stuff
-    struct timespec inter = { .tv_sec = 0, .tv_nsec = 016666667};
+    struct timespec inter = { .tv_sec = 0, .tv_nsec = 16666667};
     struct timespec start;
     struct timespec end;
     struct timespec span;
     struct timespec wait;
     //probably didn't have to have this many seperate variables
+
+    bool* playptr = malloc(sizeof(bool));
+    *playptr = false;
+    bool* playstartptr = malloc(sizeof(bool));
+    *playptr = false;
+    Bird* birdptr = newBird();
 
     window = SDL_CreateWindow(
         "Flappy Bird",                  // title
@@ -61,14 +148,18 @@ int main(int argc, char* args[]){
         timespec_get(&start,TIME_UTC);
 
         SDL_Event event;
-         while (SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) {
                 done = true;
             }
-         }
+        }
+        if (keys[SDL_SCANCODE_ESCAPE]){
+            done = true;
+        }
 
-        update(keys);
-        draw(renderer);
+        update(keys, playptr, playstartptr, birdptr);
+        draw(renderer, birdptr);
+
         SDL_UpdateWindowSurface(window);
 
         timespec_get(&end,TIME_UTC);
@@ -77,9 +168,14 @@ int main(int argc, char* args[]){
         //printf("%d\n",(int)span.tv_nsec);
         if (!(span.tv_sec>inter.tv_sec || span.tv_nsec>inter.tv_nsec)){
             wait = timespec_subtract(&inter,&span);
+            printf("%d\n",(int)wait.tv_nsec);
             nanosleep(&wait,NULL);
         }
     }
+
+    free(birdptr);
+    free(playptr);
+    free(playstartptr);
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
