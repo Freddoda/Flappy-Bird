41fff2b1321e34c2f12381f91adb329362245f7a
Freddoda <alfredgormly@gmail.com>
2026-03-31 21:58:34 +1100

    Added Pipes
    haven't made them do anything yet though

--- C/Flappy.c
+++ C/Flappy.c
@@ -28,6 +28,60 @@ void renderCircle(SDL_Renderer* renderer, int x, int y, int rad, Colour colour){
     }
 }
 
+void renderRect(SDL_Renderer* renderer, int x, int y, int w, int h, Colour colour){
+    SDL_SetRenderDrawColor(renderer, colour.r, colour.g, colour.b, 255);
+    SDL_FRect *rect = malloc(sizeof(SDL_FRect));
+    *rect = (SDL_FRect) {x,y,w,h};
+    SDL_RenderFillRect(renderer, rect);
+    free(rect);
+}
+
+typedef struct {
+    int num;
+    int x;
+    int y;
+    int gap; //from centre y
+    int width; //from centre x
+} Pipe;
+
+Pipe* newPipe(int num, int gap, int width){
+    Pipe* pipe = malloc(sizeof(Pipe));
+    *pipe = (Pipe) {num, 520*num, (rand()%(940-2*gap))+gap, gap, width};
+    return pipe;
+}
+
+void drawPipe(Pipe* pipe, SDL_Renderer* renderer){
+    renderRect(renderer, pipe->x-pipe->width, 0, 2*pipe->width, pipe->y-pipe->gap, (Colour) {0,255,0});
+    renderRect(renderer, pipe->x-pipe->width, pipe->y+pipe->gap, 2*pipe->width, 940-(pipe->y+pipe->gap), (Colour) {0,255,0});
+}
+
+typedef struct {
+    Pipe* pipe1;
+    Pipe* pipe2;
+} Pipes;
+
+Pipes* newPipes(int gap, int width){
+    Pipes* pipes = malloc(sizeof(Pipes));
+    *pipes = (Pipes) {newPipe(1,gap,width),newPipe(2,gap,width)};
+    return pipes;
+}
+
+void drawPipes(Pipes* pipes, SDL_Renderer* renderer){
+    drawPipe(pipes->pipe1, renderer);
+    drawPipe(pipes->pipe2, renderer);
+}
+
+void resetPipes(Pipes* pipes){
+    pipes->pipe1->x=pipes->pipe1->num*520;
+    pipes->pipe2->x=pipes->pipe2->num*520;
+}
+
+void DestroyPipes(Pipes* pipes){
+    free(pipes->pipe1);
+    free(pipes->pipe2);
+    free(pipes);
+}
+
 typedef struct {
     int x;
     float y;
@@ -37,7 +91,7 @@ typedef struct {
 } Bird;
 
 Bird* newBird(){
-        Bird* b = malloc(sizeof(Bird));
+    Bird* b = malloc(sizeof(Bird));
     if (b) {
         b->x=200;
         b->y=400;
@@ -52,11 +106,11 @@ void bird_jump(Bird* bird, const bool* keys){
     if (bird->jtimer>0){
         bird->jtimer--;
     } else if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_SPACE]){
-        bird->jtimer=30;
+        bird->jtimer=25;
         bird->ysp=-11;
     }
     if (bird->ysp<10){
-        bird->ysp+=0.4;
+        bird->ysp+=0.5;
     }
     bird->y += bird->ysp;
 }
@@ -78,13 +132,14 @@ void bird_draw(Bird* bird, SDL_Renderer* renderer){
     renderCircle(renderer, bird->x, bird->y, bird->rad, (Colour){255,255,0});
 }
 
-void update(const bool* keys, bool* playing, bool* playstart, Bird* bird){
+void update(const bool* keys, bool* playing, bool* playstart, Bird* bird, Pipes* pipes){
     if (*playing){
         bird_jump(bird,keys);
         if (bird_collide(bird)){
             *playing = false;
             *playstart = false;
             bird_reset(bird);
+            resetPipes(pipes);
         }
     } else {
         if (keys[SDL_SCANCODE_W] || keys[SDL_SCANCODE_SPACE]){
@@ -97,11 +152,12 @@ void update(const bool* keys, bool* playing, bool* playstart, Bird* bird){
     }
 }
 
-void draw(SDL_Renderer* renderer, Bird* bird){
+void draw(SDL_Renderer* renderer, Bird* bird, Pipes* pipes){
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     bird_draw(bird, renderer);
+    drawPipes(pipes, renderer);
 
     SDL_RenderPresent(renderer);
 }
@@ -114,6 +170,7 @@ int main(int argc, char* args[]){
     bool done = false;
     SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL3
     const bool *keys = SDL_GetKeyboardState(NULL);
+    srand(time(NULL));
 
     //framerate stuff
     struct timespec inter = { .tv_sec = 0, .tv_nsec = 16666667};
@@ -128,6 +185,7 @@ int main(int argc, char* args[]){
     bool* playstartptr = malloc(sizeof(bool));
     *playptr = false;
     Bird* birdptr = newBird();
+    Pipes* pipesptr = newPipes(110,50);
 
     window = SDL_CreateWindow(
         "Flappy Bird",                  // title
@@ -157,8 +215,8 @@ int main(int argc, char* args[]){
             done = true;
         }
 
-        update(keys, playptr, playstartptr, birdptr);
-        draw(renderer, birdptr);
+        update(keys, playptr, playstartptr, birdptr, pipesptr);
+        draw(renderer, birdptr, pipesptr);
 
         SDL_UpdateWindowSurface(window);
 
@@ -176,6 +234,7 @@ int main(int argc, char* args[]){
     free(birdptr);
     free(playptr);
     free(playstartptr);
+    DestroyPipes(pipesptr);
 
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
